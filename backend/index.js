a6a273da21d367f991128f0cb43e4535c69f685a
Yogesh Poul <yogeshpoul9999@gmail.com>
2024-08-26 23:17:38 +0530

    for netlify

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000
 app.use(cors());
 
 app.use(express.json())
-app.use('/api/v1', router)
+app.use('/.netlify/functions/app', router)
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
