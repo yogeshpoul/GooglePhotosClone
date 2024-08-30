7ab9797dde1d1c3c1c6650a6432b3b322c0308d4
Yogesh Sadashiv poul <yogesh.poul@sphinxworldbiz.com>
2024-08-30 18:43:54 +0530

    css added for modals button

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -90,13 +90,17 @@ const Modal = ({ selectedMedia, closeImage, handleImageUpdate }) => {
 
     return (
         selectedMedia && (
-            <div
-                className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50"
-            >
+            <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50">
                 <div className="relative w-full h-full flex items-center justify-center">
                     {renderContent()}
-                    {/* Buttons container */}
-                    <div className="absolute top-2 left-2 right-2 flex justify-between">
+
+                    {/* Buttons container with gradient effect */}
+                    <div
+                        className="absolute top-0 left-0 right-0 p-3 flex justify-between"
+                        style={{
+                            backgroundImage: 'linear-gradient(0deg, transparent, rgba(0, 0, 0, .38))',
+                        }}
+                    >
                         {/* Back Button */}
                         <button
                             onClick={(e) => {
@@ -115,6 +119,7 @@ const Modal = ({ selectedMedia, closeImage, handleImageUpdate }) => {
                                 <path d="M19 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H19v-2z" />
                             </svg>
                         </button>
+
                         <div className="flex gap-1">
                             {/* Share Button */}
                             <button
@@ -124,7 +129,7 @@ const Modal = ({ selectedMedia, closeImage, handleImageUpdate }) => {
                                 }}
                                 className="text-white rounded-full p-2 flex items-center justify-center"
                             >
-                                <svg width="24px" fill='white' height="24px" viewBox="0 0 24 24"><path d="M18 16c-.79 0-1.5.31-2.03.81L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.53.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.48.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.05 4.12c-.05.22-.09.45-.09.69 0 1.66 1.34 3 3 3s3-1.34 3-3-1.34-3-3-3zm0-12c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zM6 13c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm12 7c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1z"></path></svg>
+                                <svg width="24px" fill="white" height="24px" viewBox="0 0 24 24"><path d="M18 16c-.79 0-1.5.31-2.03.81L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.53.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.48.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.05 4.12c-.05.22-.09.45-.09.69 0 1.66 1.34 3 3 3s3-1.34 3-3-1.34-3-3-3zm0-12c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zM6 13c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm12 7c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1z"></path></svg>
                             </button>
 
                             {/* Delete Button */}
